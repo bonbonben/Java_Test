a3ff68b33489c2f5d0f7c87ca4f9a9b4a8518b5c
Ben Hung <hben8352@gmail.com>
2021-03-30 16:32:22 +0800

    Create random password.java

---
+++ Practice/random password.java
@@ -0,0 +1,41 @@
+// Java code to explain how to generate random password
+// Here we are using random() method of util class in Java
+import java.util.*;
+
+public class NewClass
+{
+	public static void main(String[] args)
+	{
+		// Length of your password
+		int length = 10;
+		System.out.println(random_Password(length));
+	}
+
+	static char[] random_Password(int len)
+	{
+		System.out.println("Generating password using random() : ");
+		System.out.print("Your new password is : ");
+
+		// A strong password has Capital_chars, Small_chars, numbers and symbols.
+		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+	    String symbols = "!@#$%^&*_=+-/.?<>)";
+
+		String values = Capital_chars + Small_chars + numbers + symbols;
+
+		// Using random method
+		Random rndm_method = new Random();
+
+		char[] password = new char[len];
+
+		for (int i = 0; i < len; i++)
+		{
+			// Use of charAt() method : to get character value
+			// Use of nextInt() as it is scanning the value as int
+			password[i] = values.charAt(rndm_method.nextInt(values.length()));
+
+		}
+		return password;
+	}
+}
